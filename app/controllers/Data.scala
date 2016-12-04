542ff9cb666ecabc63f97c6b4cdd615a0d5f78a5
iv <noemail@noemail.noemail>
2016-12-04 17:52:35 -0500

    trying to make this action compile.

--- app/controllers/Data.scala
+++ app/controllers/Data.scala
@@ -4,10 +4,10 @@ import play.api.data.Form
 import play.api.libs.functional.FunctionalBuilder
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
 import service.YjpsMongoData
 import storage.MongoStorage
-import yjps.models.{StoreRequest, YjpsRequest}
+import yjps.models.{StoreRequest, StoreResponse, YjpsRequest}
 
 import scala.concurrent.Future
 
@@ -23,9 +23,25 @@ object Data extends Controller {
       (JsPath \ "keyslots").read[Seq[String]]
     )(StoreRequest)
 
-  def store = Action(parse.json) {request =>
+  implicit val storeResponseWrites: Writes[StoreResponse] = new Writes[StoreResponse] {
+      def writes(storeResponse: StoreResponse) = Json.obj(
+        "data_id" -> storeResponse.data_id,
+        "keyslot_ids" -> storeResponse.keyslot_ids
+      )
+    }
 
-    val = storeRequestReads.reads(request.body)
-    dataAccess.insertObject()
+  def store: Action[JsValue] = Action(parse.json) { request =>
+    request.body
+      .asOpt
+      .map(dataAccess.insertObject)
+      .map{storeResponseFut =>
+        storeResponseFut
+          .map(storeResponse =>
+            Ok(storeResponse)
+          )
+      }
+      .getOrElse(
+        Future[Result](BadRequest)
+      )
   }
 }
